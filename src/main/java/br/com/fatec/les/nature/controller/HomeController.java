f9b8dbd0cc22e44f0f450da100a6ef9d9a630598
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-03-11 00:17:39 -0300

    feat: Adição de view para exibição dos produtos.

--- src/main/java/br/com/fatec/les/nature/controller/HomeController.java
+++ src/main/java/br/com/fatec/les/nature/controller/HomeController.java
@@ -7,9 +7,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/nature/home", method = RequestMethod.GET)
+	/**
+	 * Método reponsável por invocar a home page do sistema.
+	 * @return String contendo o nome do arquivo html a ser invocado
+	 */
+	@RequestMapping(value = "/nature", method = RequestMethod.GET)
 	public String getIndex() {
 		System.out.println("Entry point NaturÊ");
 		return "index";
 	}
+	
+	/**
+	 * Método responsável por incovar a página de exibição dos produtos do sistema. 
+	 * @return
+	 */
+	@RequestMapping(value = "/produtos", method = RequestMethod.GET)
+	public String mostrarProdutos(){
+		
+		return "produtos";
+	}
+	
+	
 }
