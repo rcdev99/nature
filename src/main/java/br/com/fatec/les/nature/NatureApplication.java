808754d4441a792e4ac8240c33ec5c29bb1c5ad8
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-05-12 12:57:25 -0300

    feat: Configurações iniciais de segurança.

--- src/main/java/br/com/fatec/les/nature/NatureApplication.java
+++ src/main/java/br/com/fatec/les/nature/NatureApplication.java
@@ -2,11 +2,11 @@ package br.com.fatec.les.nature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@ComponentScan({"br.com.fatec.les.nature.controller","br.com.fatec.les.nature.service","br.com.fatec.les.nature.storage","br.com.fatec.les.nature.service"})
+@SpringBootApplication()
+@ComponentScan({"br.com.fatec.les.nature.controller","br.com.fatec.les.nature.service","br.com.fatec.les.nature.storage",
+	"br.com.fatec.les.nature.service","br.com.fatec.les.nature.conf"})
 public class NatureApplication {
 
 	public static void main(String[] args) {
