186ddbf17f45b8abfc1507eba1d7623937f95ae3
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-06-25 11:39:18 -0300

    feat: Criou estrutura de dados para a Entidade Estoque.

---
+++ src/main/java/br/com/fatec/les/nature/model/Estoque.java
@@ -0,0 +1,94 @@
+package br.com.fatec.les.nature.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="tbl_estoque")
+public class Estoque {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="estoque_sequence")
+	@SequenceGenerator(name = "estoque_sequence", sequenceName = "est_seq", initialValue = 1, allocationSize = 1)
+	@Column(name="est_in_id")
+    private Long id;
+	
+	@OneToOne(cascade=CascadeType.ALL , fetch = FetchType.LAZY)
+	@JoinColumn(name="est_in_id_produto", referencedColumnName = "pdt_in_id")
+	private Produto produto;
+	
+	@NotNull
+	@Min(value=0)
+	private BigDecimal quantidade;
+	
+	@Column(name="est_bo_disponivel" , columnDefinition = "boolean default 'true'")
+	private boolean disponivel;
+	
+	@Transient
+	private BigDecimal valorEmEstoque;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+
+	public BigDecimal getValorEmEstoque() {
+		
+		BigDecimal valor = new BigDecimal(0);
+		
+		valor = this.produto.getValor().multiply(quantidade);
+		valor = valor.setScale(2, RoundingMode.HALF_EVEN);
+		
+		valorEmEstoque = valor;
+		return valorEmEstoque;
+	}
+
+	public void setValorEmEstoque(BigDecimal valorEmEstoque) {
+		this.valorEmEstoque = valorEmEstoque;
+	}
+	
+	
+	
+}
