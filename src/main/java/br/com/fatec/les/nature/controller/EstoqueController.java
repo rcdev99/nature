1866079ec7ea3e4f72bf524ac670e93146f7fdf1
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-06-26 08:50:56 -0300

    feat: Criou view para possibilitar a edição de informações de um item de estoque.

--- src/main/java/br/com/fatec/les/nature/controller/EstoqueController.java
+++ src/main/java/br/com/fatec/les/nature/controller/EstoqueController.java
@@ -2,9 +2,12 @@ package br.com.fatec.les.nature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.fatec.les.nature.model.Estoque;
 import br.com.fatec.les.nature.service.EstoqueService;
 
 @Controller
@@ -20,6 +23,19 @@ public class EstoqueController {
 		ModelAndView mView = new ModelAndView("dashboard-adm-estoque");
 		mView.addObject("estoque", estoqueService.buscarTodos());
 		mView.addObject("valorTotal", estoqueService.valorTotalEmEstoque());
+		
+		return mView;
+	}
+	
+	@RequestMapping(value="/gerenciar/{id.estoque}")
+	public ModelAndView alterarEstoque(@PathVariable("id.estoque") Long id, RedirectAttributes redirectAttributes){
+		
+		ModelAndView mView = new ModelAndView("dashboard-adm-estoque-detalhes");
+		
+		Estoque estoque = estoqueService.buscarPorId(id);
+		mView.addObject("estoque", estoque);
+		
+		
 		
 		return mView;
 	}
