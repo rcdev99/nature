edbe990a2737839f38d92f540a42d9a81e66b2bd
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-06-26 10:33:31 -0300

    feat: Implementou view para edição da quantidade de itens em estoque.

--- src/main/java/br/com/fatec/les/nature/controller/EstoqueController.java
+++ src/main/java/br/com/fatec/les/nature/controller/EstoqueController.java
@@ -1,9 +1,12 @@
 package br.com.fatec.les.nature.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,7 +31,7 @@ public class EstoqueController {
 	}
 	
 	@RequestMapping(value="/gerenciar/{id.estoque}")
-	public ModelAndView alterarEstoque(@PathVariable("id.estoque") Long id, RedirectAttributes redirectAttributes){
+	public ModelAndView alterarEstoque(@PathVariable("id.estoque") Long id){
 		
 		ModelAndView mView = new ModelAndView("dashboard-adm-estoque-detalhes");
 		
@@ -38,6 +41,25 @@ public class EstoqueController {
 		
 		
 		return mView;
+	}
+	
+	@RequestMapping(value="/editar", method = RequestMethod.POST)
+	public ModelAndView editarItemEstoque(Long id, BigDecimal quantidade, RedirectAttributes redirectAttributes) {
+		
+		ModelAndView mView = new ModelAndView("redirect:/estoque/listar");
+		
+		Estoque estoque = estoqueService.buscarPorId(id);
+		
+		if(estoque != null) {
+			estoque.setQuantidade(quantidade);
+			estoqueService.salvar(estoque);
+			redirectAttributes.addFlashAttribute("success", "Alteração realizada com sucesso");
+		}else{
+			redirectAttributes.addFlashAttribute("error", "Não foi possível concluir a alteração");
+		}
+		
+		return mView;
+		
 	}
 	
 }
