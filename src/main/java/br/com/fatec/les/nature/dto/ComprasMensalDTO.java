721203d8ab9e3f5a591d628d9ee840c71e8e4985
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-07-07 19:05:28 -0300

    feat: Implementou busca por periodo no relatório de analises da view administrativa.

--- src/main/java/br/com/fatec/les/nature/dto/ComprasMensalDTO.java
+++ src/main/java/br/com/fatec/les/nature/dto/ComprasMensalDTO.java
@@ -1,6 +1,8 @@
 package br.com.fatec.les.nature.dto;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class ComprasMensalDTO {
 
@@ -19,6 +21,65 @@ public class ComprasMensalDTO {
 		
 	}
 
+	public ComprasMensalDTO(Integer qtd, String data) {
+		
+		String array[] = new String [2];
+		array = data.split("/");
+		
+		Integer ano = Integer.valueOf(array[0]);
+		Integer mes = Integer.valueOf(array[1]);
+		
+		this.mes = new GregorianCalendar(ano, (mes - 1), 1);
+		this.qtdCompras = qtd;
+	}
+	
+	/**
+	 * Método que compõe a lista de período incluindo zero nos meses em que não houve registro de vendas dentro do período selecionado
+	 * @param inicio - String que representa a data do início do período selecionado
+	 * @param fim - String que representa a data do fim do período selecionado
+	 * @param compras - List<ComprasMensalDTO> contendo as compras realizadas no período selecionado
+	 * @return
+	 */
+	public static List<ComprasMensalDTO> comporListaPorPeriodo(String inicio, String fim, List<ComprasMensalDTO> compras){
+		
+		List<ComprasMensalDTO> comprasPorPeriodo = new ArrayList<ComprasMensalDTO>();
+		
+		//Array de string contendo: (0)-ano, (1)-mês, (2)-dia
+		String begin[] = new String[3];
+		String end[] = new String[3];
+		
+		begin = inicio.split("-");
+		end = fim.split("-");
+		
+		GregorianCalendar dt_inicio = new GregorianCalendar(Integer.valueOf(begin[0]), (Integer.valueOf(begin[1]) - 1), Integer.valueOf(begin[2])); 
+		GregorianCalendar dt_fim = new GregorianCalendar(Integer.valueOf(end[0]), (Integer.valueOf(end[1]) - 1), Integer.valueOf(end[2]));
+		//Obs.: É necessário decrementar uma unidade do mês, pois o range de meses deste objeto vai de 0 a 11
+		
+		while(dt_fim.compareTo(dt_inicio) > 0) {
+			
+			boolean encontrou = false;
+			
+			//Percorre a lista verificando se há no mês atual do iterador um registro correpondente na lista passada como parâmetro
+			for (ComprasMensalDTO compra : compras) {
+				if((compra.getMes().get(GregorianCalendar.YEAR) == dt_inicio.get(GregorianCalendar.YEAR)) 
+						&& (compra.getMes().get(GregorianCalendar.MONTH) == dt_inicio.get(GregorianCalendar.MONTH))) {
+					comprasPorPeriodo.add(compra);
+					encontrou = true;
+				}
+			}
+			
+			//Caso não encontre, atribui qtd igual a zero àquele mês
+			if(!encontrou) {
+				comprasPorPeriodo.add(new ComprasMensalDTO(0, (dt_inicio.get(GregorianCalendar.MONTH) + 1), dt_inicio.get(GregorianCalendar.YEAR)));
+			}
+			
+			//Incremento da data de início da busca
+			dt_inicio.add(GregorianCalendar.MONTH, 1);
+		}
+	
+		return comprasPorPeriodo;
+	}
+	
 	public Integer getQtdCompras() {
 		return qtdCompras;
 	}
